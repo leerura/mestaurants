1d34f2d9ed84f3a5eed1a818bd07a9410bd54d0f
LEE <sw09112@yonsei.ac.kr>
2024-07-07 00:41:46 +0900

    before url

--- mestaurants/list/models.py
+++ mestaurants/list/models.py
@@ -1,4 +1,15 @@
-from django.db import models
+from django.db import models #Django 모델 사용!!
 
+class Restaurant(models.Model): #Django 모델 만들었음
+    name = models.CharField(max_length=100) #식당 이름
+    location = models.CharField(max_length=100) # 위치
+    description = models.TextField(blank=True, null=True) #설명
+    category = models.CharField(max_length=50, blank=True) #카테고리
+    average_rating = models.FloatField(default=0.0) #평점
+    created_at = models.DateTimeField(auto_now_add=True)  # 생성 일자
+    updated_at = models.DateTimeField(auto_now=True)  # 수정 일자
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+
+# blank와 null의 차이점이 뭐죠????
--- mestaurants/list/views.py
+++ mestaurants/list/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Restaurant
+from .serializers import RestaurantSerializer
+
+class RestaurantListCreate(generics.ListCreateAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+
+class RestaurantRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
 
 # Create your views here.
